67dc1b624eacb91af34746d07d3759e6d7ae5f29
hiday <hiday1979@gmail.com>
2017-01-14 19:27:44 +0200

    start of ruots

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,60 +1,90 @@
-$(document).ready(function () {
-  var trigger = $('.hamburger'),
-      overlay = $('.overlay'),
-     isClosed = false;
+$(document).ready(function() {
+    /* valadision*/
 
-    trigger.click(function () {
-      hamburger_cross();      
+    var trigger = $('.hamburger'),
+        overlay = $('.overlay'),
+        isClosed = false;
+
+    trigger.click(function() {
+        hamburger_cross();
     });
 
     function hamburger_cross() {
 
-      if (isClosed == true) {          
-        overlay.hide();
-        trigger.removeClass('is-open');
-        trigger.addClass('is-closed');
-        isClosed = false;
-      } else {   
-        overlay.show();
-        trigger.removeClass('is-closed');
-        trigger.addClass('is-open');
-        isClosed = true;
-      }
-  }
-  
-  $('[data-toggle="offcanvas"]').click(function () {
-        $('#wrapper').toggleClass('toggled');
-  });  
-});
-$(window).bind("load", function() { 
-       
-       var footerHeight = 0,
-           footerTop = 0,
-           $footer = $("#footer");
-           
-       positionFooter();
-       
-       function positionFooter() {
-       
-                footerHeight = $footer.height();
-                footerTop = ($(window).scrollTop()+$(window).height()-footerHeight)+"px";
-       
-               if ( ($(document.body).height()+footerHeight) < $(window).height()) {
-                   $footer.css({
-                        position: "absolute"
-                   }).animate({
-                        top: footerTop
-                   })
-               } else {
-                   $footer.css({
-                        position: "static"
-                   })
-               }
-               
-       }
+        if (isClosed == true) {
+            overlay.hide();
+            trigger.removeClass('is-open');
+            trigger.addClass('is-closed');
+            isClosed = false;
+        } else {
+            overlay.show();
+            trigger.removeClass('is-closed');
+            trigger.addClass('is-open');
+            isClosed = true;
+        }
+    }
 
-       $(window)
-               .scroll(positionFooter)
-               .resize(positionFooter)
-               
+    $('[data-toggle="offcanvas"]').click(function() {
+        $('#wrapper').toggleClass('toggled');
+    });
 });
+$(window).bind("load", function() {
+
+    var footerHeight = 0,
+        footerTop = 0,
+        $footer = $("#footer");
+
+    positionFooter();
+
+    function positionFooter() {
+
+        footerHeight = $footer.height();
+        footerTop = ($(window).scrollTop() + $(window).height() - footerHeight) + "px";
+
+        if (($(document.body).height() + footerHeight) < $(window).height()) {
+            $footer.css({
+                position: "absolute"
+            }).animate({
+                top: footerTop
+            })
+        } else {
+            $footer.css({
+                position: "static"
+            })
+        }
+
+    }
+
+    $(window)
+        .scroll(positionFooter)
+        .resize(positionFooter)
+
+});
+
+function checkPass() {
+    //Store the password field objects into variables ...
+    var pass1 = document.getElementById('pass1');
+    var pass2 = document.getElementById('pass2');
+    //Store the Confimation Message Object ...
+    var message = document.getElementById('confirmMessage');
+    //Set the colors we will be using ...
+    var goodColor = "#66cc66";
+    var badColor = "#ff6666";
+    //Compare the values in the password field 
+    //and the confirmation field
+    if (pass1.value == pass2.value) {
+        //The passwords match. 
+        //Set the color to the good color and inform
+        //the user that they have entered the correct password 
+        pass2.style.backgroundColor = goodColor;
+        message.style.color = goodColor;
+        message.innerHTML = "Passwords Match!"
+    } else {
+        //The passwords do not match.
+        //Set the color to the bad color and
+        //notify the user.
+        pass2.style.backgroundColor = badColor;
+        message.style.color = badColor;
+        message.innerHTML = "Passwords Do Not Match!"
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,65 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('login', { title: 'GamesWebSite' });
 });
 
+/* GET users table page. */
+/*
+router.get('/initDb', function(req, res, next) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        if (err) {
+            return res.render('error', {
+                error: err,
+                message: err.message
+            });
+        }
+        var SQL = "CREATE TABLE users(id SERIAL UNIQUE, userName TEXT NOT NULL, password TEXT NOT NULL, email TEXT NOT NULL)";
+        client.query(SQL, function(err, result) {
+            if (err) {
+                return res.render('error', {
+                    error: err,
+                    message: err.message
+                });
+            }
+            done();
+            res.render('index', { title: "succeesfully added users table" });
+        });
+    });
+});
+*/
+
+/* POST Add Users page. */
+router.post('/addUser', function(req, res, next) {
+    //
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var email = req.body.email;
+    var password = req.body.password;
+
+
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        if (err) {
+            return res.render('error', {
+                error: err,
+                message: err.message
+            });
+        }
+        var SQL = "INSERT INTO users(firstName, lastName, email, password) VALUES($1, $2, $3, $4);";
+        client.query(SQL, [firstName, lastName, email, numOfChild], function(err, result) {
+            if (err) {
+                return res.render('error', {
+                    error: err,
+                    message: err.message
+                });
+            }
+            done();
+            res.render('login', {
+                title: "succeesfully added user",
+                users: result.rows
+
+            });
+        });
+    });
+});
 module.exports = router;
