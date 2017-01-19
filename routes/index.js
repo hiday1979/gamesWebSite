794e01f1968de1ce06e433d35e203371033e8e45
hiday <hiday1979@gmail.com>
2017-01-19 17:45:12 +0200

    ewq

--- routes/index.js
+++ routes/index.js
@@ -136,7 +136,7 @@ router.post('/login', function(req, res, next) {
                 })
             }
             
-          else  if (email == res.result.email && password == res.result.email){
+          else  if (email == users.result.email && password == users.result.email){
             res.render('index', {
                 title: "Wellcame Back"
              })
